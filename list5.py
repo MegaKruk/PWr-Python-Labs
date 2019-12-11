42449d5c6a501e01c47c1d5b4afd6323f67e34fb
MegaKruk <filip.mazur.dev@gmail.com>
2019-12-11 01:18:35 +0100

    Add files via upload

--- list5.py
+++ list5.py
@@ -1,32 +1,19 @@
 import random
+from random import shuffle
 import math
+import numpy as np
+import matplotlib.pylab as plt
 import os
 import imageio as io
-import matplotlib.pylab as plt
 from matplotlib.colors import ListedColormap
 
-#iterations = 20 # iteracje
-#speed_limit = 3 # limit prędkosci
-#brake_chance = 0.1 # szansa na hamowanie
-#density = 0.3 # gestosc
-#size = 20 # rozmiar autostrady
+#iterations = 100
+#speed_limit = 5
+#brake_chance = 0.1 (probablity of slowing down)
+#density = 0.3 (car density)
+#size = 20
 
-#0. Stworzyć autostradę, która przechowuje samochód [obiekt] lub puste miejsce [None]
-#1. Zainicjować autostradę samochodami tak, że ilość samochodów to gęstość * rozmiar autostrady. Samochód[i] będzie na autostrada[i]
-#2. Sprawdzamy jaka jest odległość między tym a następnym samochodem (do momentu aż dojdziemy do Vmax). 
-#3. Jeżeli odległość == 0 ORAZ obecna prędkość == 0 to nic nie rób
-#LUB
-#Jeżeli odległość < obecna prędkość ORAZ obecna prędkość > 0 to zwolnij do prędkości == odległość
-#LUB
-#Jeżeli obecna prędkość > 0 i wylosowano losowe hamowanie to zwolnij o 1
-#LUB
-#Jeżeli odległość > obecna prędkość ORAZ obecna prędkość < Vmax to przyspiesz o 1
-#4. Stworzyć tymczasową autostradę (domyślnie pustą) i wrzucamy każdy samochód na nowe miejsce o indeksie [(stara pozycja + obecna prędkość) % rozmiar autostrady]
-#5. Obliczyć średnią prędkość na  autostradzie
-#6. Na matrycę wrzuć [obecną iterację + stan autostrady (None będzie zapisywany jako 0, obiekt samochód jako 1 + średnia prędkość) 
-#7. Jak skończymy całą matrycę to animujemy
-
-
+#class "Car", which stores the car object and its functionality
 class Car:
     
     def __init__(self, current_speed, max_speed, brake_chance):
@@ -36,20 +23,14 @@ class Car:
         
     def change_speed(self, new_speed):
         self.current_speed = new_speed
-        
-#    def random_brake(self):
-#        if (self.current_speed > 0) and (random.random() <  self.brake_chance):
-#            self.change_speed(self.current_speed - 1) 
-    
+           
     def get_current_speed(self):
         return self.current_speed
     
     def get_max_speed(self):
         return self.max_speed
-    
-    def to_string(self):
-        return ('cs:\t' + str(self.current_speed) + '\tsl:\t' + str(self.max_speed) + '\tp:\t' + str(self.brake_chance))
-    
+
+#clas "FreewaySim", which stores car objects and empty spaces (Nones) 
 class FreewaySim:
     
     simulation_matrix = []
@@ -64,11 +45,15 @@ class FreewaySim:
         self.freeway = [None] * self.size
         self.cars_number = 0
     
+    #initiating the freeway with cars - the number of cars is the density * size of the freeway
+    #car[i] is on the freeway[i]
     def init_cars(self):
         self.cars_number = math.floor(self.size * self.cars_density)
         for i in range(self.cars_number):
             self.freeway[i] = Car(0, self.speed_limit, self.brake_chance)
-    
+        shuffle(self.freeway)
+            
+    #we check the distance between one car and the next one (until we reach Vmax).
     def car_motion(self):
         iterator = 0
         for field in self.freeway:
@@ -77,26 +62,36 @@ class FreewaySim:
                 while (self.freeway[(iterator + next_pointer) % self.size] == None) and (next_pointer <= self.freeway[iterator].get_max_speed()):
                     next_pointer += 1
                 distance = next_pointer - 1
+                
+                #if distance = 0 and current speed = 0 then do nothing (case of "traffic jam")
                 if distance == 0 and self.freeway[iterator].get_current_speed() == 0:
                     pass
-                #Jeżeli odległość < obecna prędkość ORAZ obecna prędkość > 0 to zwolnij do prędkości == odległość
+                
+                #if distance < current speed and current speed > 0 then slow down the car to speed = distance
                 elif distance < self.freeway[iterator].get_current_speed() and self.freeway[iterator].get_current_speed() > 0:
                     self.freeway[iterator].change_speed(distance)
-                #Jeżeli obecna prędkość > 0 i wylosowano losowe hamowanie to zwolnij o 1
-#                elif self.freeway[iterator].get_current_speed() > 0 and (random.random() < self.freeway[iterator].brake_chance):
-#                    self.freeway[iterator].change_speed(self.freeway[iterator].current_speed - 1)
-                #Jeżeli odległość > obecna prędkość ORAZ obecna prędkość < Vmax to przyspiesz o 1
+                    
+                #if distance > current speed and current speed < Vmax
                 elif distance > self.freeway[iterator].get_current_speed() and self.freeway[iterator].get_current_speed() < self.freeway[iterator].get_max_speed():
+                    
+                    #and if random slowing down was selected
                     if random.random() < self.freeway[iterator].brake_chance:
+                        
+                        #and if we are still driving
                         if self.freeway[iterator].get_current_speed() > 0:
+                            
+                            #slow down by 1
                             self.freeway[iterator].change_speed(self.freeway[iterator].current_speed - 1)
+                            
+                    #or speed up by 1       
                     else:
                         self.freeway[iterator].change_speed(self.freeway[iterator].get_current_speed() + 1) 
                 else:
                     pass
-#                print(self.freeway[iterator].get_current_speed())
             iterator += 1
-        #4. Stworzyć tymczasową autostradę (domyślnie pustą) i wrzucamy każdy samochód na nowe miejsce o indeksie [(stara pozycja + obecna prędkość) % rozmiar autostrady]
+            
+        #creating a temporary freeway (empty -> Nones) and putting each car to a new place 
+        #with the index [(old position + current speed) % freeway size]
         temp_freeway = [None] * self.size
         for i in range (self.size):
             if self.freeway[i] != None:
@@ -107,14 +102,19 @@ class FreewaySim:
         sum_of_speed_average = 0.0
         for index in range(self.iterations):
             self.car_motion()
-            #5. Obliczyć średnią prędkość na  autostradzie
+            
+            #calculating the average velocity on the freeway
             speed_sum = 0.0
             for i in range(self.size):
                 if self.freeway[i] != None:
                     speed_sum += self.freeway[i].get_current_speed()
             speed_avg = round((speed_sum / float(self.cars_number)), 3)
             sum_of_speed_average += speed_avg
-            #6. Na matrycę wrzuć [obecną iterację + stan autostrady (None będzie zapisywany jako 0, obiekt samochód jako 1 + średnia prędkość) 
+            
+            #creating the matrix with:
+            #- current iteration 
+            #- current location of cars on the freeway (None = 0, car = 1) 
+            #- average velocity
             freeway_state = []
             for i in range(self.size):
                 if self.freeway[i] != None:
@@ -122,49 +122,101 @@ class FreewaySim:
                 else:
                     freeway_state.append(0)
             self.simulation_matrix.append([index] + freeway_state + [speed_avg])
-        AVG = sum_of_speed_average / float(self.iterations)
+        #AVG = sum_of_speed_average / float(self.iterations)
         for i in range(len(self.simulation_matrix)):
             print(self.simulation_matrix[i])
-        return AVG
-            
-    def animate(self, directory='Images'): # co x sekund plotuje i-ty stan symulacji
-        i = 0
-        fig = plt.subplots()
-        files = []
-        for line in self.simulation_matrix:
-             file_name = os.path.join(directory, str(i) + '.png')
-             files.append(file_name)
-             fig.savefig(file_name, dpi=100)
-             i += 1
+        #return AVG
+        return self.simulation_matrix
+    
+    #ODKOMENTOWAĆ AVG JAK ZROBIE GIFY I WYWALIĆ RETURN SELF...
+
+#-----------------------------------------------------------------------------------            
+def animate(states, directory='Images'):
+    segregation_colours = ListedColormap(['w', 'k'])
+    i = 0
+    fig, ax = plt.subplots()
+    fig.set_size_inches(40, 15)
+    ax.axis('off')
+    ax.set_xticks([])
+    ax.set_yticks([])
+    files = []
+    for line in states:
+        ax.clear()
+        ax.set_xticks([])
+        ax.set_yticks([])
+        ax.matshow(line, cmap=segregation_colours)
+        file_name = os.path.join(directory, str(i) + '.png')
+        files.append(file_name)
+        fig.savefig(file_name, dpi=100)
+        i += 1
+    with io.get_writer('animation.gif', mode='I', duration=0.2) as w:
+        for file in files:
+            w.append_data(io.imread(file))
         
-        with io.get_writer('animation.gif', mode='I', duration=0.3) as w:
-            for file in files:
-                w.append_data(io.imread(file))
-        
-            
-#    def animate(states, directory='Images'):
-#    segregation_colours = ListedColormap(['k', 'b', 'r'])
-#    i = 0
-#    fig, ax = plt.subplots()
-#    ax.axis('off')
-##    ax.set_xticks([])
-##    ax.set_yticks([])
-#    files = []
-#    for state in states:
-#        ax.clear()
-#        ax.set_xticks([])
-#        ax.set_yticks([])
-#        ax.matshow(state, cmap=segregation_colours)
-#        file_name = os.path.join(directory, str(i) + '.png')
-#        files.append(file_name)
-#        fig.savefig(file_name, dpi=100)
-#        i += 1
-#    with io.get_writer('animation.gif', mode='I', duration=0.3) as w:
-#        for file in files:
-#            w.append_data(io.imread(file))
-        
-sim1 = FreewaySim(50, 0.1, 0.1, 5, 40)  #iterations, cars_density, brake_chance, speed_limit, size  
+
+#-------------------------------------------------------------------------------------------------
+#iterations, cars_density, brake_chance, speed_limit, size          
+sim1 = FreewaySim(200, 0.1, 0.35, 5, 100)
 sim1.init_cars()
 AVG1 = sim1.start_simulation()
-print(AVG1)
-#sim1.animate()
+#(AVG1)
+
+for i in AVG1:
+    del i[0]
+    
+for j in AVG1:
+    del j[-1]
+
+board=[]    
+for k in range(len(AVG1)):
+    board.append([])
+
+for n in range(len(AVG1)):
+     board[n].append(AVG1[n])  
+     
+ars=[]
+for i in range(len(board)):
+    ars.append(np.array(board[i]))
+
+animate(ars)   
+#-------------------------------------------------------------------------------------------------
+
+#Exercise 2
+        
+#CarDensities = np.arange(0.1, 1, 0.05)
+#
+#Runs2 = []
+#for q in CarDensities:
+#    Runs2.append(FreewaySim(100, q, 0.2, 5, 100))    
+#VelocityAverages2 = []    
+#for a in range(len(Runs2)):
+#    Runs2[a].init_cars()
+#    VelocityAverages2.append(Runs2[a].start_simulation())
+#    
+#Runs5 = []
+#for q in CarDensities:
+#    Runs5.append(FreewaySim(100, q, 0.5, 5, 100))
+#VelocityAverages5 = []    
+#for a in range(len(Runs5)):
+#    Runs5[a].init_cars()
+#    VelocityAverages5.append(Runs5[a].start_simulation())
+#    
+#Runs7 = []
+#for q in CarDensities:
+#    Runs7.append(FreewaySim(100, q, 0.7, 5, 100))    
+#VelocityAverages7 = []    
+#for a in range(len(Runs7)):
+#    Runs7[a].init_cars()
+#    VelocityAverages7.append(Runs7[a].start_simulation())
+#
+#fig, ax = plt.subplots()    
+#ax.plot(CarDensities, VelocityAverages2, '-b', label='p = 0.2')
+#ax.plot(CarDensities, VelocityAverages5, '-r', label='p = 0.5')
+#ax.plot(CarDensities, VelocityAverages7, '-g', label='p = 0.7') 
+#ax.legend(loc='upper right', frameon=False)
+#
+#plt.title('Average velocity for probabilities of slowing down', fontsize=12)
+#plt.xlabel('car density', fontsize=10)
+#plt.ylabel('average velocity', fontsize=10)  
+#fig
+
