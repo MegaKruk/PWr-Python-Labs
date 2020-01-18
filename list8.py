92f072de4112f19a652d115d59815f7543cdd5aa
MegaKruk <filip.mazur.dev@gmail.com>
2020-01-18 13:15:51 +0100

    Add files via upload

--- list8.py
+++ list8.py
@@ -1,91 +1,89 @@
 import matplotlib.pyplot as pyplot
 
-def productDiffusion2(inSize, imSize, p, q, f, years, dt):
-    InList = []
-    ImList = []
-    totalList= []
-    timeList = []
+def produce_diffusion(innovators_population, imitators_population, p, q, f, years, dt):
+    time_list = []
+    innovators_list = []
+    imitators_list = []
+    all_agents_list= []
+    innovators_list2 = [0]
+    imitators_list2 = [0]
+    all_agents_list2 = [0]
     
-    influenced_adopters_Before= 0
-    imitated_adopters_Before = 0
-    total_adopters_Before = 0
     t = 0
-    InList.append(influenced_adopters_Before)
-    ImList.append(imitated_adopters_Before)
-    totalList.append(total_adopters_Before)
-    timeList.append(t)
-    InList1 = [0]
-    ImList1 = [0]
-    totalList1 = [0]
+    influenced_adopters_before = 0
+    imitating_adopters_before = 0
+    all_adopters_before = 0
+
+    innovators_list.append(influenced_adopters_before)
+    imitators_list.append(imitating_adopters_before)
+    all_agents_list.append(all_adopters_before)
+    time_list.append(t)
     
-    for i in range(0, int(years/dt)+1):
-        newly_influenced = InList[-1] + p * (1 - InList[-1])  *dt + q * InList[-1] * (1 - InList[-1]) * dt   #differential equation for newly influenced agents
-        InList.append(newly_influenced)
-        newly_imitated = ImList[-1] + p * (1 - ImList[-1]) * dt + f * q * InList[-1] * (1 - ImList[-1])*dt + (1 - f) * q * ImList[-1] * (1 - ImList[-1]) * dt    #differential equation for new imitators
-        ImList.append(newly_imitated)
+    for i in range(0, int(years / dt) + 1):
+        newly_influenced = innovators_list[-1] + p * (1 - innovators_list[-1])  * dt + q * innovators_list[-1] * (1 - innovators_list[-1]) * dt   # differential equation for newly influenced agents
+        innovators_list.append(newly_influenced)
+        newly_imitated = imitators_list[-1] + p * (1 - imitators_list[-1]) * dt + f * q * innovators_list[-1] * (1 - imitators_list[-1]) * dt + (1 - f) * q * imitators_list[-1] * (1 - imitators_list[-1]) * dt    #differential equation for new imitators
+        imitators_list.append(newly_imitated)
         
         t = t + dt
-        timeList.append(t)
+        time_list.append(t)
         
-        total_new_influenced = (newly_influenced - influenced_adopters_Before) / dt
-        InList1.append(total_new_influenced)
+        total_new_influenced = (newly_influenced - influenced_adopters_before) / dt
+        innovators_list2.append(total_new_influenced) #innovators_list for figure 2 is probably counted wrong :c
         
-        total_new_imitated = (newly_imitated - imitated_adopters_Before) / dt
-        ImList1.append(total_new_imitated)
+        total_new_imitated = (newly_imitated - imitating_adopters_before) / dt
+        imitators_list2.append(total_new_imitated)
         
         total_new_adoptions = ((newly_influenced) + (newly_imitated))
-        totalList.append(total_new_adoptions)
-        total_adoptions = (total_new_adoptions - total_adopters_Before) / dt
-        totalList1.append(total_adoptions)
+        all_agents_list.append(total_new_adoptions)
+        total_adoptions = (total_new_adoptions - all_adopters_before) / dt
+        all_agents_list2.append(total_adoptions)
 
-        influenced_adopters_Before = newly_influenced
-        imitated_adopters_Before = newly_imitated
-        total_adopters_Before = total_new_adoptions
+        influenced_adopters_before = newly_influenced
+        imitating_adopters_before = newly_imitated
+        all_adopters_before = total_new_adoptions
         
-    for i in range(len(InList)): # second loop to change fraction into percentage
-        InList[i] = InList[i] * inSize
-        InList1[i] = InList1[i] * inSize
-        ImList[i] = ImList[i] * imSize
-        ImList1[i] = ImList1[i] * imSize
-        totalList1[i] = InList1[i] + ImList1[i]
-        totalList[i] = InList[i] + ImList[i]
-    return InList, ImList, InList1, ImList1, timeList, totalList, totalList1
-def main():
+    for i in range(len(innovators_list)): # second loop to change fraction into percentage
+        innovators_list[i] = innovators_list[i] * innovators_population
+        innovators_list2[i] = innovators_list2[i] * innovators_population
+        imitators_list[i] = imitators_list[i] * imitators_population
+        imitators_list2[i] = imitators_list2[i] * imitators_population
+        all_agents_list2[i] = innovators_list2[i] + imitators_list2[i]
+        all_agents_list[i] = innovators_list[i] + imitators_list[i]
+#        time_list - time for X axis
+#        all_agents_list - list of all agents
+#        innovators_list - list of rates of changes for the innovators
+#        imitators_list - list of rates of changes for the imitators
+#        lists with 2 are for figure 2
+    return innovators_list, imitators_list, innovators_list2, imitators_list2, time_list, all_agents_list, all_agents_list2
+
+def bass_diffusion_model(innovators_population, imitators_population, p, q, f, years, dt):
     
-#    Parameters:
-#        p - the chance of the product adoption
-#        q - interaction between adopters and non-adopters
-#        years - number of years
-#        dt - the quant of time
-#        inSize - population size of innovators
-#        imSize - population size of imitators
-#        f - the strength with which non-adopters impress adopters
-#    Return Values:
-#        timeList - time for X axis
-#        aList - list of adopters
-#        totalList - list of all agents
-#        Inlist1 - list of rates of changes for the innovators
-#        ImList1 - list of rates of changes for the imitators
-    
-    inSize = 600
-    imSize = 400
-    p = 0.03
-    q = 0.38
-    f = 0.6
-    years = 30
-    dt = 0.01
-    InList, ImList, InList1, ImList1, timeList, totalList, totalList1 = productDiffusion2(inSize, imSize, p, q, f, years, dt)
+    innovators_list, imitators_list, innovators_list2, imitators_list2, time_list, all_agents_list, all_agents_list2 = produce_diffusion(innovators_population, imitators_population, p, q, f, years, dt)
     pyplot.figure(1)
-    pyplot.plot(timeList, InList, label = 'Innovators')
-    pyplot.plot(timeList, ImList, label = 'Imitators')
-    pyplot.plot(timeList, totalList, label = 'Total')
+    pyplot.plot(time_list, innovators_list, label = 'Innovators')
+    pyplot.plot(time_list, imitators_list, label = 'Imitators')
+    pyplot.plot(time_list, all_agents_list, label = 'Total')
     pyplot.legend()
     
     pyplot.figure(2)
-    pyplot.plot(timeList, InList1, label = 'Innovators')
-    pyplot.plot(timeList, ImList1, label = 'Imitators')
-    pyplot.plot(timeList, totalList1, label = 'Total')
+    pyplot.plot(time_list, innovators_list2, label = 'Innovators')
+    pyplot.plot(time_list, imitators_list2, label = 'Imitators')
+    pyplot.plot(time_list, all_agents_list2, label = 'Total')
     pyplot.legend()
     pyplot.show()
+
+
+if __name__ == "__main__":
+
+#    Parameters:
+#    innovators_population - population size of innovators
+#    imitators_population - population size of imitators
+#    p - the chance of the product adoption
+#    q - interaction between adopters and non-adopters
+#    f - the strength with which non-adopters impress adopters
+#    years - number of years
+#    dt - the quant of time
+    bass_diffusion_model(600, 400, 0.03, 0.38, 0.6, 30, 0.01)
     
-main()
+    
